057a0ed19b10ce86453f15f58f1ece16ffa55c00
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:02:42 +0300

    Add files via upload

---
+++ 4.100/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _4._100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            
+            Console.WriteLine("Первое число: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Второе число: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Второе число: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            double d = 0;
+
+            if (a > b || a > c) d += a;
+            if (b > a || b > c) d += b;
+            if (c > b || c > a) d += c;
+
+            Console.WriteLine($"Сумма двух наибольших чисел {d}");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+    }
+}
